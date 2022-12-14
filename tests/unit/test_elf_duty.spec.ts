03f90ced6451798406392130d9f7197b802e6a69
Raúl Castro Estévez <raulcastroestevez@helindata.com>
2022-12-14 12:57:38 +0100

    Adapt Tests for Part 2 of each puzzle (4-6)

--- tests/unit/test_elf_duty.spec.ts
+++ tests/unit/test_elf_duty.spec.ts
@@ -1,60 +1,72 @@
 import { ElfDuties } from "@domain/elfduty";
 
-describe("Test Day 4 - Elf Duties Overlap", () =>{
+describe("Test Day 4 - Elf Duties Fully  Overlap", () =>{
+    it("test_first_elf_duty_fully_overlaps_second_elf_duty", () => {
+        const elfCouple = ElfDuties.generateElfDuties(1, 1000, 25, 250);
 
-    it("test_first_elf_duty_overlaps_second_elf_duty", () => {
-        const elf_couple = ElfDuties.generateElfDuties(1, 1000, 25, 250);
-
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeTruthy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeTruthy();
     });
 
-    it("test_second_elf_duty_overlaps_first_elf_duty", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(25, 250, 1, 1000);
+    it("test_second_elf_duty_fully_overlaps_first_elf_duty", () => { 
+        const elfCouple = ElfDuties.generateElfDuties(25, 250, 1, 1000);
 
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeTruthy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeTruthy();
     });
 
-    it("test_first_elf_duty_overlaps_second_elf_duty_if_beginnings_match", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(25, 1000, 25, 250);
+    it("test_first_elf_duty_fully_overlaps_second_elf_duty_if_beginnings_match", () => { 
+        const elfCouple = ElfDuties.generateElfDuties(25, 1000, 25, 250);
 
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeTruthy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeTruthy();
     });
 
-    it("test_first_elf_duty_overlaps_second_elf_duty_if_ends_match", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(1, 1000, 25, 1000);
+    it("test_first_elf_duty_fully_overlaps_second_elf_duty_if_ends_match", () => { 
+        const elfCouple = ElfDuties.generateElfDuties(1, 1000, 25, 1000);
         
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeTruthy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeTruthy();
     });
 
-    it("test_second_elf_duty_overlaps_first_elf_duty_if_beginnings_match", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(1, 250, 1, 1000);
+    it("test_second_elf_duty_fully_overlaps_first_elf_duty_if_beginnings_match", () => { 
+        const elfCouple = ElfDuties.generateElfDuties(1, 250, 1, 1000);
         
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeTruthy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeTruthy();
     });
 
-    it("test_second_elf_duty_overlaps_first_elf_duty_if_ends_match", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(25, 1000, 1, 1000);
+    it("test_second_elf_duty_fully_overlaps_first_elf_duty_if_ends_match", () => { 
+        const elfCouple = ElfDuties.generateElfDuties(25, 1000, 1, 1000);
 
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeTruthy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeTruthy();
     });
 
     it("test_first_elf_duty_does_not_overlap_second_elf_duty", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(1, 25, 250, 1000);
+        const elfCouple = ElfDuties.generateElfDuties(1, 25, 250, 1000);
 
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeFalsy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeFalsy();
     });
 
     it("test_second_elf_duty_does_not_overlap_first_elf_duty", () => { 
-        const elf_couple = ElfDuties.generateElfDuties(250, 1000, 1, 25);
+        const elfCouple = ElfDuties.generateElfDuties(250, 1000, 1, 25);
         
-        expect(ElfDuties.areElfsDutiesOverlaping(elf_couple[0], elf_couple[1])).toBeFalsy();
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1])).toBeFalsy();
+    });
+});
+
+describe("Test Day 4 - Elf Duties Partial Overlap", () =>{
+    it("test_first_elf_duty_partial_overlaps_second_elf_duty", () => {
+        const elfCouple = ElfDuties.generateElfDuties(1, 200, 25, 250);
+
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1], false)).toBeTruthy();
     });
 
+    it("test_second_elf_duty_partial_overlaps_first_elf_duty", () => { 
+        const elfCouple = ElfDuties.generateElfDuties(25, 250, 1, 200);
+
+        expect(ElfDuties.areElfsDutiesOverlaping(elfCouple[0], elfCouple[1], false)).toBeTruthy();
+    });
 });
 
 describe("Test Day 4 - Counter", () =>{
-    it("Test counter", () => {
-        const goodDutySample = [
+    function generateSample(): Array<Array<number>>{
+        return [
             [2, 4, 6, 8],
             [2, 3, 4, 5],
             [5, 7, 7, 9],
@@ -62,8 +74,18 @@ describe("Test Day 4 - Counter", () =>{
             [6, 6, 4, 6],
             [2, 6, 4, 8]
         ];
+    }
 
-        expect(ElfDuties.countOverlapDuties(goodDutySample)).toBe(2);
+    it("Test fully overlap counter", () => {
+        const goodDutySample = generateSample();
+
+        expect(ElfDuties.countOverlapDuties(goodDutySample, true)).toBe(2);
+    });
+
+    it("Test fully overlap counter", () => {
+        const goodDutySample = generateSample();
+
+        expect(ElfDuties.countOverlapDuties(goodDutySample, false)).toBe(4);
     });
 
 });
