724989a81dfe4970adeb32dd9d80f6519ef43a9a
minirulo <rcastroest@gmail.com>
2022-12-18 21:59:15 +0100

    Test functionality of the device

--- tests/unit/test_device_stream.spec.ts
+++ tests/unit/test_device_stream.spec.ts
@@ -1,4 +1,5 @@
 import { Device } from "@domain/device";
+import { InvalidMessageError } from "@domain/errors";
 
 describe("Test Day 6 - Device message parser", () => {
     it("Test device parses packet", () => {
@@ -6,12 +7,26 @@ describe("Test Day 6 - Device message parser", () => {
         const device = new Device();
 
         expect(device.parseMessage(message)).toBe(7);
-    })
+    });
 
     it("Test device parses message", () => {
         const message = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
         const device = new Device(14);
 
         expect(device.parseMessage(message)).toBe(19);
-    })
+    });
+
+    it("Test device parses when packet is at the end", () => {
+        const message = "aaaaaaaaaaabcd";
+        const device = new Device(4);
+
+        expect(device.parseMessage(message)).toBe(14);
+    });
+
+    it("Test device throws on non existing message / packet", () => {
+        const message = "aaaaaaaaaaaaaa";
+        const device = new Device(4);
+
+        expect(() => device.parseMessage(message)).toThrowError(InvalidMessageError);
+    });
 });
